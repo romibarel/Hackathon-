4e7744e158f798e51c8642f3ae65d9aa2a82d142
romibarel <romib19999@gmail.com>
2021-03-30 22:08:20 +0300

    shokty - start login logic

--- Hestory/app/src/main/java/com/example/hestory/HomeActivity.java
+++ Hestory/app/src/main/java/com/example/hestory/HomeActivity.java
@@ -2,15 +2,34 @@ package com.example.hestory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final Button addButton = (Button) findViewById(R.id.addButton);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+ //       final ImageButton addButton = (ImageButton) findViewById(R.id.addButton);
         setContentView(R.layout.activity_home);
+
+  /*      addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingProgressBar.setVisibility(View.VISIBLE);
+                openAddStoryActivity();
+                Log.d("success", "opened reg");
+            }
+        }); */
+    }
+
+    private void openAddStoryActivity(){
+
     }
 }
