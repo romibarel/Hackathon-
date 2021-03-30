d73cdde60d04d6c2f39d7c646cf20d7394d9133c
romibarel <romib19999@gmail.com>
2021-03-30 22:53:15 +0300

    starting to work on homepage

---
+++ Hestory/app/src/main/java/com/example/hestory/Stories.java
@@ -0,0 +1,44 @@
+package com.example.hestory;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Stories {
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    @SerializedName("totalResult")
+    @Expose
+    private int totalResult;
+
+    @SerializedName("stories")
+    @Expose
+    private List<Story> stories;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getTotalResult() {
+        return totalResult;
+    }
+
+    public void setTotalResult(int totalResult) {
+        this.totalResult = totalResult;
+    }
+
+    public List<Story> getStories() {
+        return stories;
+    }
+
+    public void setStories(List<Story> stories) {
+        this.stories = stories;
+    }
+}
